d7e669964a1c55ca1e4838ddc03eb4a45ea3fbe1
Dumitru <dima50066@gmail.com>
2025-04-01 14:44:57 +0200

    work with template, named slots+slots+collections

--- frontend/src/components/IButton/IButton.vue
+++ frontend/src/components/IButton/IButton.vue
@@ -1,5 +1,5 @@
 <template>
   <button class="bg-[#FFA279] rounded-xl py-3 px-10 text-white font-bold -tracking-wider">
-    Start now
+    <slot></slot>
   </button>
 </template>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,7 +1,12 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#f3743d',
+        gray: '#939393'
+      }
+    }
   },
   plugins: []
-}
+};
