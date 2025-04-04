d981db0e7b52789de547f1aa3763ae6f7141e52f
Dumitru <dima50066@gmail.com>
2025-04-04 14:25:56 +0200

    events, watch + v-model

--- frontend/src/components/FavoritePlaces/FavoritePlaces.vue
+++ frontend/src/components/FavoritePlaces/FavoritePlaces.vue
@@ -2,30 +2,30 @@
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
 import { ref } from 'vue'
+import IInput from '../IInput/IInput.vue'
 
-const counter = ref(0)
-const user = ref({ name: 'Tom', age: 14 })
+const buttonVariant = ref('gradient')
 
-const increment = () => {
-  counter.value += 1
-}
-
-const changeUserName = () => {
-  user.value.name = 'Harry Potter'
+const changeButtonVariant = () => {
+  buttonVariant.value = buttonVariant.value === 'gradient' ? 'outlined' : 'gradient'
 }
 </script>
 
 <template>
   <div class="px-6">
-    <div class="text-gray">User name: {{ user.name }}</div>
-    <div class="text-gray mb-4">Додані маркери ({{ counter }})</div>
+    <div class="text-gray mb-4">Додані маркери</div>
+    <div class="py-5">
+      <IInput label="Some label" />
+      <a href="/" class="text-black">CLick me</a>
+    </div>
     <slot name="label"></slot>
     <slot name="list">
       <FavoritePlace v-for="n in 4" :key="n" />
     </slot>
 
     <slot></slot>
-    <IButton class="w-full" @click="changeUserName">Change name</IButton>
-    <IButton class="w-full mt-10" @click="increment">Додати маркер</IButton>
+    <IButton class="w-full mt-10" :variant="buttonVariant" @click="changeButtonVariant"
+      >Додати маркер</IButton
+    >
   </div>
 </template>
