708578677a2faa602a7075e6a1a5c517b08581cd
Dmytro <153439599+dima50066@users.noreply.github.com>
2025-06-25 15:25:44 +0200

    update example

--- frontend/src/api/auth/authService.ts
+++ frontend/src/api/auth/authService.ts
@@ -3,8 +3,7 @@ import { fetchOrCreateUser } from '../user/user';
 import { router } from '../../router';
 
 export const loginWithGoogle = () => {
-  window.location.href = `${import.meta.env.VITE_API_URL}/auth`;
-};
+  window.location.href = `${(import.meta as any).env.VITE_API_URL}/auth`;};
 
 export const initUser = async () => {
   const authStore = useAuthStore();
