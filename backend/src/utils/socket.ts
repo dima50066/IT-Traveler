208dc919913956cdce190501faf225a57be30951
Dmytro <153439599+dima50066@users.noreply.github.com>
2025-08-04 15:46:59 +0200

    fix: update CORS configuration to allow multiple origins

--- backend/src/utils/socket.ts
+++ backend/src/utils/socket.ts
@@ -11,7 +11,7 @@ let io: Server;
 export const initSocket = (server: HttpServer) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: [env("FRONTEND_URL"), "http://localhost:5173"],
       methods: ["GET", "POST"],
     },
   });
